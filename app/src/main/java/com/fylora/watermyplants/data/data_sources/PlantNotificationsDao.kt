9e14f9444a56bf55df36ed0f699dae2875070249
fylora <fylora189@gmail.com>
2023-12-02 17:17:39 +0200

    Added plants notifications dao

---
+++ app/src/main/java/com/fylora/watermyplants/data/data_sources/PlantNotificationsDao.kt
@@ -0,0 +1,16 @@
+package com.fylora.watermyplants.data.data_sources
+
+import android.app.Notification
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Upsert
+
+@Dao
+interface PlantNotificationsDao {
+    @Query("SELECT * FROM notifications")
+    fun getAllNotifications(): List<Notification>
+
+    @Upsert
+    fun upsertNotification(notification: Notification)
+}
